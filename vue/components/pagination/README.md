092ade9046176af90cb6526e81c838328bfecabf
文强 <qiang.wen1@pactera.com>
2019-04-08 16:42:22 +0800

    #pagination 基于element-ui的分页组件封装

---
+++ vue/components/pagination/README.md
@@ -0,0 +1,2 @@
+# pagination
+通过封装element-ui的pagination达到项目内复用
---
+++ vue/components/pagination/pagination.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="paging">
+        <el-pagination
+                small
+                :page-size="size"
+                @current-change="handleCurrentChange"
+                layout="total, prev, pager, next, jumper"
+                :total="total">
+        </el-pagination>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            total: { type: Number },
+            size: { type: Number },
+        },
+        name: 'pagination',
+        data() {
+            return {
+
+            };
+        },
+        methods: {
+            handleCurrentChange(val) {
+                this.$emit('getPageIndex', val);
+            }
+        },
+        created() {
+            console.log(this.total, this.size);
+        }
+    };
+</script>
+
+<style lang="scss">
+    .paging{
+        float: right;
+        margin-top: 0.2rem;
+        .el-pagination__editor.el-input .el-input__inner{
+            width: 100%;
+            border: 1px solid #DCDFE6;
+        }
+        .el-pagination .el-select .el-input .el-input__inner{
+            width: 100%;
+            border: 1px solid #DCDFE6;
+        }
+    }
+
+</style>
